541f9f4ad2f6f0869e025cffe6b089f5207b783c
nikitdwivedi <nikitdwivedi1997@gmail.com>
2023-10-22 05:27:21 +0530

    deploy.sh test

--- app/index.js
+++ app/index.js
@@ -6,5 +6,5 @@ const port = process.env.APPID||4007;
 const IP = process.env.IP || '127.0.0.1';
 const server = http.createServer(app);
 server.listen(port, () => {
-    console.log(`Fablo Server listening at http://${IP}:${port}`)
+    console.log(`Fablo listening at http://${IP}:${port}`)
 });
